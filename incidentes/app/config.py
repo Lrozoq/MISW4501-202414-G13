77b5b73909019b689fcee809302c8ed000b20ce6
santiago <santiagowilchesv@gmail.com>
2024-09-14 18:26:02 -0500

    config DB

--- incidentes/app/config.py
+++ incidentes/app/config.py
@@ -16,11 +16,11 @@ DB_PORT_REPLICA = os.getenv("DB_PORT_REPLICA","5432")
 
 PROJECT_ID = os.getenv("GCP_PROJECT_ID", "abcall-435301")
 TOPIC_ID = os.getenv("GCP_TOPIC_ID", "incidents-db-sync")
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "../service-account.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "service-account.json"
 
 REDIS_HOST = os.getenv("REDIS_HOST","localhost")
 REDIS_PORT = os.getenv('DOCKER_REDIS_PORT', 6379)
 REDIS_SERVICE_NAME = os.getenv('DOCKER_REDIS_SERVICE_NAME')
 
-DB_SOCKET_PATH = os.getenv("DB_SOCKET_PATH_PRIMARY", "")
+DB_SOCKET_PATH_PRIMARY = os.getenv("DB_SOCKET_PATH_PRIMARY", "")
 DB_SOCKET_PATH_REPLICA = os.getenv("DB_SOCKET_PATH_REPLICA", "")
--- incidentes/app/database.py
+++ incidentes/app/database.py
@@ -7,14 +7,14 @@ from app.models import Incidente
 from google.cloud import pubsub_v1
 
 # URL para la base de datos primaria
-if config.DB_SOCKET_PATH:
-    SQLALCHEMY_DATABASE_URL_PRIMARY = f"mysql+mysqlconnector://{config.DB_USER}:{config.DB_PASSWORD}@/{config.DB_NAME}?unix_socket={config.DB_SOCKET_PATH}"
+if config.DB_SOCKET_PATH_PRIMARY:
+    SQLALCHEMY_DATABASE_URL_PRIMARY = f"mysql+mysqlconnector://{config.DB_USER}:{config.DB_PASSWORD}@/{config.DB_NAME}?unix_socket={config.DB_SOCKET_PATH_PRIMARY}"
 else:
     SQLALCHEMY_DATABASE_URL_PRIMARY = f"mysql+mysqlconnector://{config.DB_USER}:{config.DB_PASSWORD}@{config.DB_HOST}:{config.DB_PORT}/{config.DB_NAME}"
 
 # URL para la base de datos réplica
 if config.DB_SOCKET_PATH_REPLICA:
-    SQLALCHEMY_DATABASE_URL_PRIMARY = f"mysql+mysqlconnector://{config.DB_USER}:{config.DB_PASSWORD}@{config.DB_HOST}:{config.DB_PORT}/{config.DB_NAME}"
+    SQLALCHEMY_DATABASE_URL_REPLICA = f"mysql+mysqlconnector://{config.DB_USER}:{config.DB_PASSWORD}@/{config.DB_NAME}?unix_socket={config.DB_SOCKET_PATH_PRIMARY}"
 else:
     SQLALCHEMY_DATABASE_URL_REPLICA = f"mysql+mysqlconnector://{config.DB_USER}:{config.DB_PASSWORD}@{config.DB_HOST_REPLICA}:{config.DB_PORT_REPLICA}/{config.DB_NAME_REPLICA}"
 
